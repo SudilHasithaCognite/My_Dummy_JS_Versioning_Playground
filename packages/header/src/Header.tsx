89d34149103510fd3332594c1aa0f2bf98619c73
SudilHasithaCognite <sudil.abeyagunasekera@cognite.com>
2024-11-18 15:36:06 +0530

    feat(test): add a feature to v2 rc

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { HeaderFeature } from './HeaderFeature';
+
 
 export function Header() {
+    const featureText = "This is a header feature"
     return (
+        <>
         <header>
             Lerna is the original monorepo tool!
         </header>
+        <HeaderFeature text={featureText}/>
+        </>
     );
 }
