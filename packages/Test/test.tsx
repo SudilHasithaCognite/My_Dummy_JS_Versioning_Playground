b19904c875f58fb5d5d581ec49c360c0f8065c35
SudilHasithaCognite <sudil.abeyagunasekera@cognite.com>
2024-11-18 21:58:49 +0530

    feat(test): add a feature directly to v2 main [release]

--- packages/Test/test.tsx
+++ packages/Test/test.tsx
@@ -1,1 +1,11 @@
-// dummy commit to rerun the main
+//  main feature 
+export function testing(n){
+    let prev = 0
+    let next = 1
+    for(let i; i <= n; i++){
+        let temp = prev 
+        prev = next
+        next = temp + next
+    }
+    return next
+}
